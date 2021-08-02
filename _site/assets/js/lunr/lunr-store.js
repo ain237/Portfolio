3b2949e18edbb34448299003000a09f98d288fc9
ain237 <87855526+ain237@users.noreply.github.com>
2021-08-02 18:09:43 +0300

    Add files via upload

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Sports Analytics - Building a xG Model with Linear Regression (Python, Sklearn, Seaborn)",
+        "excerpt":"1. Introduction Soccer match, like a match in any sports competition is an one-time performance faced with many uncertainties. On a bad day an excellent player will miss many goals, and some bad players might win out of pure luck. To understand the real performance of players, many soccer analysts...","categories": [],
+        "tags": [],
+        "url": "/xg-post/",
+        "teaser": null
+      }]
